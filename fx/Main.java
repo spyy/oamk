1934864cf2cb4055c6f6db6375a28411ac632e62
spyy <sami.pyy@gmail.com>
2015-01-27 14:03:58 +0200

    rectancle node

--- fx/Main.java
+++ fx/Main.java
@@ -1,10 +1,10 @@
- 
+
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-
  
 public class Main extends Application {
  
@@ -12,7 +12,12 @@ public class Main extends Application {
    public void start(Stage stage) {
        Group root = new Group();
        Scene scene = new Scene(root, 500, 700, Color.BLACK);
-       stage.setTitle("FX harjoitus");
+ 
+       Rectangle r = new Rectangle(25,25,250,250);
+       r.setFill(Color.BLUE);
+       root.getChildren().add(r);
+ 
+       stage.setTitle("JavaFX harjoituksia");
        stage.setScene(scene);
        stage.show();
    }
